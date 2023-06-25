6ea6eb0adacdfc1dd691651df6d09c3f153925e7
Sheepish Shepherd <36808587+SheepishShepherd@users.noreply.github.com>
2023-06-25 17:08:24 -0400

    Bringing changes from 1.4.4 to 1.4 (#6)
    * init commit for 1.4.4 porting
    
    * Visibility booleans are now properties
    
    * MusicUISystem cleanup
    
    * scrollbar changes
    
    * magic number removed
    
    * Modplayer cleanup
    
    * cleaned up MusicPlayerUI Update hook
    
    * moved AllMusic from tMusicPlayer to MusicUISystem
    
    * Expanded and improved the MusicData structure
    
    * Added 'The Aether' music
    
    * Mini-mode player will display name and mod on hover
    
    * moved canPlay from MusicPlayerUI to MusicData
    
    * improvements to panels and buttons
    
    * OrganizeSelection cleanup
    
    * fixed dragging panels while hovering over a button
    
    * MusicBoxSlot initializers + fix display mini box
    
    * removed unused references
    
    * ModPlayer cleanup
    
    * Differentiated Mod.Name and Mod.DisplayName (and NoChatTags)
    
    * reworked several UI fields into properties
    
    * minor cleanup
    
    * fixed button logic
    
    * Updated logic for FindNext/Prev
    
    * removing a music box correctly updates display slot
    
    * removing a music box while selected will stop music from playing
    
    * MusicBoxSlot rework and clean up
    
    * CanPlay checks for BoxIsCollected and BoxResearched
    
    * fixed inability to open chat with selection panel active
    
    * DisplayBox title and mod properly display
    
    * fixed not being able to stop currently playing music from selection panel
    
    * improved recording music feature
    
    * reduced ListenModeData usage in MusicUI.Update
    
    * right-clicking the search bar now updates selection panel
    
    * List view song names now use item rarity color
    
    * Empty selection slots now use a default blue slot
    
    * Created inheritable class to reduce common variable instances
    
    * fixed scrollbar being unclickable
    
    * list view entries no longer overflow off the panel
    
    * Changed toggle music player ModHotkey to ModCommand
    
    * more readability for view mode related tasks
    
    * made use of the 'default to list view' config
    
    * fixed being able to scroll when hovering over buttons
    
    * removed unneeded mouseInterface updates
    
    * removed unneeded parenthesis
    
    * Localized debug messages
    
    * Localized UIElement hover texts
    
    * MusicData.Name now takes LocalizedTexts into account
    
    * removed Instance.MusicUI from within MusicUISystem
    
    * fixed some issues with hidden by default configs
    
    * minor tweaks
    
    * added special case where music has no assigned music box
    
    * removed unnecessary UI initialization tasks
    
    * PostAddRecipes -> PostSetupContent
    
    * removed composer support
    
    Modders can add their composers to the item tooltips. No need for them to make a mod call to do what they can already do without this mod.
    
    * UIElements cleanup
    
    * adjusted default positions for music player and selection panels
    
    * cleaned up configs
    
    * Added pop-out text for list view
    
    * RemoveChatTags is now static
    
    * HoverButton refItem -> refData
    
    * applied more IsRecording uses
    
    * removed some unused references
    
    * handled some vs code suggestions
    
    * v1.0
    
    * reverted 1.4.4 port requirements and fixed some issues
    
    * applied config translations

--- MusicData.cs
+++ MusicData.cs
@@ -1,40 +1,74 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.GameContent.UI;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
 
 namespace tMusicPlayer
 {
 	internal class MusicData {
-		internal int music;
-		internal int musicbox;
-		internal string mod;
-		internal string name;
-		internal int mainMusicBox2;
+		/// <summary> The music ID assigned. This is used as a unique identifier. </summary>
+		internal int MusicID { get; init; }
 
-		public override string ToString() => $"{name} from {mod} [Item#{musicbox}] -- [MusicID#{music}]";
-		
+		/// <summary> The numerical value used for 'Main.musicBox2', the value used for equipping music boxes as an accessory. </summary>
+		internal int OutputValue { get; init; }
+
+		/// <summary> The item ID of the music box that plays this music. </summary>
+		internal int MusicBox { get; init; }
+
+		/// <summary> The internal name of the mod that the music originates from. </summary>
+		internal string Mod { get; init; }
+
+		/// <summary> The name provided to the music box. </summary>
+		internal string Name { get; init; }
+
+		/// <summary> The display name provided by the item's localization. </summary>
+		internal string DisplayName => Name.Contains('(') ? Name.Substring(Name.IndexOf("(") + 1).Replace(")", "") : Name;
+
+		/// <summary> Determines if the Music Player is able to play this music. </summary>
+		internal bool CanPlay(MusicPlayerPlayer modplayer) => modplayer.BoxIsCollected(MusicBox) || modplayer.BoxResearched(MusicBox);
+
+		/// <summary> Gets the index of this MusicData within <see cref="MusicUISystem.AllMusic"/></summary>
+		internal int GetIndex => MusicUISystem.Instance.AllMusic.IndexOf(this);
+
+		/// <summary> The display name of the mod. </summary>
+		internal string Mod_DisplayName => ModLoader.TryGetMod(Mod, out Mod mod) ? mod.DisplayName : Mod;
+
+		internal string Mod_DisplayName_NoChatTags => MusicUISystem.RemoveChatTags(Mod_DisplayName);
+
+		internal Color MusicBox_Rarity => ItemRarity.GetColor(ContentSamples.ItemsByType[MusicBox].rare);
+
+		public override string ToString() => $"[#{MusicID}] [i:{MusicBox}] [{Mod}] {DisplayName}";
+
+		// Unknown method; used as a substitute for music without assigned music boxes
+		internal MusicData() {
+			this.MusicID = -1;
+			this.OutputValue = -1;
+			this.MusicBox = ItemID.MusicBox;
+
+			this.Mod = "???";
+			this.Name = Language.GetText("Mods.tMusicPlayer.MusicData.UnknownBox").Value;
+		}
+
 		// Vanilla method
 		internal MusicData(int music, int musicbox, int mainMusicBox2) {
-			this.music = music;
-			this.musicbox = musicbox;
-			this.mainMusicBox2 = mainMusicBox2;
+			this.MusicID = music;
+			this.OutputValue = mainMusicBox2;
+			this.MusicBox = musicbox;
 
-			string itemNameValue = Lang.GetItemNameValue(musicbox);
-			if (itemNameValue.Contains("Otherworldly")) {
-				this.mod = "Terraria Otherworld";
-			}
-			else {
-				this.mod = "Terraria";
-			}
-			this.name = itemNameValue.Substring(itemNameValue.IndexOf("(") + 1).Replace(")", "");
+			this.Mod = music >= 58 && music <= 84 ? "Terraria Otherworld" : "Terraria";
+			this.Name = Lang.GetItemName(musicbox).Value;
 		}
 
 		// Mod method
 		public MusicData(int music, int musicbox, string mod, string name) {
-			this.music = music;
-			this.musicbox = musicbox;
-			this.mod = mod;
-			this.name = name;
+			this.MusicID = music;
+			this.OutputValue = music;
+			this.MusicBox = musicbox;
 
-			mainMusicBox2 = music;
+			this.Mod = mod;
+			this.Name = ItemLoader.GetItem(musicbox).DisplayName.GetTranslation(Language.ActiveCulture);
 		}
 	}
 }
--- MusicUISystem.cs
+++ MusicUISystem.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Reflection;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
@@ -12,26 +11,134 @@ using Terraria.UI;
 
 namespace tMusicPlayer
 {
+	[Autoload(Side = ModSide.Client)]
 	internal class MusicUISystem : ModSystem {
 		public static MusicUISystem Instance { get; private set; }
 
 		internal UserInterface MP_UserInterface;
 		internal MusicPlayerUI MusicUI;
+
+		internal MusicData UnknownMusic;
+		internal List<MusicData> AllMusic;
+		public Dictionary<int, int> itemToMusicReference;
+		internal Dictionary<string, List<int>> RegisteredMusic;
 		internal const int MaxUnrecordedBoxes = 20;
 
 		internal string UIHoverText = "";
-		internal Color UIHoverTextColor = default;
+		internal Color? UIHoverTextColor = null;
 
 		public override void Load() {
 			Instance = this;
 
 			// Setup the Music Player UI.
-			if (!Main.dedServ) {
-				MusicUI = new MusicPlayerUI();
-				MusicUI.Activate();
-				MP_UserInterface = new UserInterface();
-				MP_UserInterface.SetState(MusicUI);
-			}
+			MP_UserInterface = new UserInterface();
+			MusicUI = new MusicPlayerUI();
+			MusicUI.Activate();
+			MP_UserInterface.SetState(MusicUI);
+
+			// This grabs the entire dictionary of MODDED music-to-musicbox correlations. Code provided by Jopojelly. Thank you, Jopo!
+			FieldInfo itemToMusicField = typeof(MusicLoader).GetField("itemToMusic", BindingFlags.Static | BindingFlags.NonPublic);
+			itemToMusicReference = (Dictionary<int, int>)itemToMusicField.GetValue(null);
+
+			UnknownMusic = new MusicData();
+
+			// A list that contains all the MusicData we need, including mod and name.
+			AllMusic = new List<MusicData> {
+				#region 1.3 Music Boxes
+				new MusicData(MusicID.OverworldDay, ItemID.MusicBoxOverworldDay, 0),
+				new MusicData(MusicID.Eerie, ItemID.MusicBoxEerie, 1),
+				new MusicData(MusicID.Night, ItemID.MusicBoxNight, 2),
+				new MusicData(MusicID.Title, ItemID.MusicBoxTitle, 3),
+				new MusicData(MusicID.Underground, ItemID.MusicBoxUnderground, 4),
+				new MusicData(MusicID.Boss1, ItemID.MusicBoxBoss1, 5),
+				new MusicData(MusicID.Jungle, ItemID.MusicBoxJungle, 6),
+				new MusicData(MusicID.Corruption, ItemID.MusicBoxCorruption, 7),
+				new MusicData(MusicID.UndergroundCorruption, ItemID.MusicBoxUndergroundCorruption, 8),
+				new MusicData(MusicID.TheHallow, ItemID.MusicBoxTheHallow, 9),
+				new MusicData(MusicID.Boss2, ItemID.MusicBoxBoss2, 10),
+				new MusicData(MusicID.UndergroundHallow, ItemID.MusicBoxUndergroundHallow, 11),
+				new MusicData(MusicID.Boss3, ItemID.MusicBoxBoss3, 12),
+				new MusicData(MusicID.Snow, ItemID.MusicBoxSnow, 13),
+				new MusicData(MusicID.Space, ItemID.MusicBoxSpace, 14),
+				new MusicData(MusicID.Crimson, ItemID.MusicBoxCrimson, 15),
+				new MusicData(MusicID.Boss4, ItemID.MusicBoxBoss4, 16),
+				new MusicData(MusicID.AltOverworldDay, ItemID.MusicBoxAltOverworldDay, 17),
+				new MusicData(MusicID.Rain, ItemID.MusicBoxRain, 18),
+				new MusicData(MusicID.Ice, ItemID.MusicBoxIce, 19),
+				new MusicData(MusicID.Desert, ItemID.MusicBoxDesert, 20),
+				new MusicData(MusicID.Ocean, ItemID.MusicBoxOcean, 21),
+				new MusicData(MusicID.Dungeon, ItemID.MusicBoxDungeon, 22),
+				new MusicData(MusicID.Plantera, ItemID.MusicBoxPlantera, 23),
+				new MusicData(MusicID.Boss5, ItemID.MusicBoxBoss5, 24),
+				new MusicData(MusicID.Temple, ItemID.MusicBoxTemple, 25),
+				new MusicData(MusicID.Eclipse, ItemID.MusicBoxEclipse, 26),
+				new MusicData(MusicID.Mushrooms, ItemID.MusicBoxMushrooms, 27),
+				new MusicData(MusicID.PumpkinMoon, ItemID.MusicBoxPumpkinMoon, 28),
+				new MusicData(MusicID.AltUnderground, ItemID.MusicBoxAltUnderground, 29),
+				new MusicData(MusicID.FrostMoon, ItemID.MusicBoxFrostMoon, 30),
+				new MusicData(MusicID.UndergroundCrimson, ItemID.MusicBoxUndergroundCrimson, 31),
+				new MusicData(MusicID.LunarBoss, ItemID.MusicBoxLunarBoss, 32),
+				new MusicData(MusicID.MartianMadness, ItemID.MusicBoxMartians, 33),
+				new MusicData(MusicID.PirateInvasion, ItemID.MusicBoxPirates, 34),
+				new MusicData(MusicID.Hell, ItemID.MusicBoxHell, 35),
+				new MusicData(MusicID.TheTowers, ItemID.MusicBoxTowers, 36),
+				new MusicData(MusicID.GoblinInvasion, ItemID.MusicBoxGoblins, 37),
+				new MusicData(MusicID.Sandstorm, ItemID.MusicBoxSandstorm, 38),
+				new MusicData(MusicID.OldOnesArmy, ItemID.MusicBoxDD2, 39),
+				#endregion
+				#region 1.4 Music Boxes
+				new MusicData(MusicID.WindyDay, ItemID.MusicBoxWindyDay, 40),
+				new MusicData(MusicID.SlimeRain, ItemID.MusicBoxSlimeRain, 41),
+				new MusicData(MusicID.SpaceDay, ItemID.MusicBoxSpaceAlt, 42),
+				new MusicData(MusicID.OceanNight, ItemID.MusicBoxOceanAlt, 43),
+				new MusicData(MusicID.TownDay, ItemID.MusicBoxTownDay, 44),
+				new MusicData(MusicID.TownNight, ItemID.MusicBoxTownNight, 45),
+				new MusicData(MusicID.DayRemix, ItemID.MusicBoxDayRemix, 46),
+				new MusicData(MusicID.MenuMusic, ItemID.MusicBoxTitleAlt, 47),
+				new MusicData(MusicID.Monsoon, ItemID.MusicBoxStorm, 48),
+				new MusicData(MusicID.Graveyard, ItemID.MusicBoxGraveyard, 49),
+				new MusicData(MusicID.JungleUnderground, ItemID.MusicBoxUndergroundJungle, 50),
+				new MusicData(MusicID.JungleNight, ItemID.MusicBoxJungleNight, 51),
+				new MusicData(MusicID.QueenSlime, ItemID.MusicBoxQueenSlime, 52),
+				new MusicData(MusicID.EmpressOfLight, ItemID.MusicBoxEmpressOfLight, 53),
+				new MusicData(MusicID.DukeFishron, ItemID.MusicBoxDukeFishron, 54),
+				new MusicData(MusicID.MorningRain, ItemID.MusicBoxMorningRain, 55),
+				new MusicData(MusicID.ConsoleMenu, ItemID.MusicBoxConsoleTitle, 56),
+				new MusicData(MusicID.UndergroundDesert, ItemID.MusicBoxUndergroundDesert, 57),
+				new MusicData(MusicID.Credits, ItemID.MusicBoxCredits, 85),
+				new MusicData(MusicID.Deerclops, ItemID.MusicBoxDeerclops, 86),
+				//new MusicData(MusicID.Shimmer, ItemID.MusicBoxShimmer, 87),
+				#endregion
+				#region Otherworld Music
+				new MusicData(MusicID.OtherworldlyRain, ItemID.MusicBoxOWRain, 58),
+				new MusicData(MusicID.OtherworldlyDay, ItemID.MusicBoxOWDay, 59),
+				new MusicData(MusicID.OtherworldlyNight, ItemID.MusicBoxOWNight, 60),
+				new MusicData(MusicID.OtherworldlyUnderground, ItemID.MusicBoxOWUnderground, 61),
+				new MusicData(MusicID.OtherworldlyDesert, ItemID.MusicBoxOWDesert, 62),
+				new MusicData(MusicID.OtherworldlyOcean, ItemID.MusicBoxOWOcean, 63),
+				new MusicData(MusicID.OtherworldlyMushrooms, ItemID.MusicBoxOWMushroom, 64),
+				new MusicData(MusicID.OtherworldlyDungeon, ItemID.MusicBoxOWDungeon, 65),
+				new MusicData(MusicID.OtherworldlySpace, ItemID.MusicBoxOWSpace, 66),
+				new MusicData(MusicID.OtherworldlyUnderworld, ItemID.MusicBoxOWUnderworld, 67),
+				new MusicData(MusicID.OtherworldlySnow, ItemID.MusicBoxOWSnow, 68),
+				new MusicData(MusicID.OtherworldlyCorruption, ItemID.MusicBoxOWCorruption, 69),
+				new MusicData(MusicID.OtherworldlyUGCorrption, ItemID.MusicBoxOWUndergroundCorruption, 70),
+				new MusicData(MusicID.OtherworldlyCrimson, ItemID.MusicBoxOWCrimson, 71),
+				new MusicData(MusicID.OtherworldlyUGCrimson, ItemID.MusicBoxOWUndergroundCrimson, 72),
+				new MusicData(MusicID.OtherworldlyIce, ItemID.MusicBoxOWUndergroundSnow, 73),
+				new MusicData(MusicID.OtherworldlyUGHallow, ItemID.MusicBoxOWUndergroundHallow, 74),
+				new MusicData(MusicID.OtherworldlyEerie, ItemID.MusicBoxOWBloodMoon, 75),
+				new MusicData(MusicID.OtherworldlyBoss2, ItemID.MusicBoxOWBoss2, 76),
+				new MusicData(MusicID.OtherworldlyBoss1, ItemID.MusicBoxOWBoss1, 77),
+				new MusicData(MusicID.OtherworldlyInvasion, ItemID.MusicBoxOWInvasion, 78),
+				new MusicData(MusicID.OtherworldlyTowers, ItemID.MusicBoxOWTowers, 79),
+				new MusicData(MusicID.OtherworldlyLunarBoss, ItemID.MusicBoxOWMoonLord, 80),
+				new MusicData(MusicID.OtherworldlyPlantera, ItemID.MusicBoxOWPlantera, 81),
+				new MusicData(MusicID.OtherworldlyJungle, ItemID.MusicBoxOWJungle, 82),
+				new MusicData(MusicID.OtherworldlyWoF, ItemID.MusicBoxOWWallOfFlesh, 83),
+				new MusicData(MusicID.OtherworldlyHallow, ItemID.MusicBoxOWHallow, 84)
+				#endregion
+			};
 		}
 
 		public override void Unload() {
@@ -39,28 +146,26 @@ namespace tMusicPlayer
 			MusicUI = null;
 		}
 
-		public override void PostAddRecipes() {
-			// This grabs the entire dictionary of MODDED music-to-musicbox correlations. Code provided by Jopojelly. Thank you, Jopo!
-			FieldInfo field = typeof(MusicLoader).GetField("itemToMusic", BindingFlags.Static | BindingFlags.NonPublic);
-			Dictionary<int, int> itemToMusicReference = (Dictionary<int, int>)field.GetValue(null);
+		public override void UpdateUI(GameTime gameTime) {
+			if (MP_UserInterface?.CurrentState != null)
+				MP_UserInterface?.Update(gameTime);
+		}
 
+		public override void PostSetupContent() {
 			if (itemToMusicReference != null) {
 				// Go through each key in the Modded MusicBox dictionary and attempt to add them to MusicData.
 				foreach (KeyValuePair<int, int> music in itemToMusicReference) {
-					int itemID = music.Key;
-					int musicID = music.Value;
-
-					if (!ContentSamples.ItemsByType.TryGetValue(itemID, out Item item))
+					if (!ContentSamples.ItemsByType.TryGetValue(music.Key, out Item item))
 						continue; // If the item does not exist, move onto the next pair
 
-					string name = item.Name.Contains("(") ? item.Name.Substring(item.Name.IndexOf("(") + 1).Replace(")", "") : item.Name;
-					string displayName = item.ModItem == null ? "Terraria" : item.ModItem.Mod.DisplayName;
+					string name = item.ModItem == null ? item.Name : item.ModItem.Name;
+					string modSource = item.ModItem == null ? "Terraria" : item.ModItem.Mod.Name;
 
-					if (!tMusicPlayer.AllMusic.Exists(x => x.musicbox == itemID)) {
-						tMusicPlayer.AllMusic.Add(new MusicData(musicID, itemID, displayName, name));
+					if (!AllMusic.Exists(x => x.MusicBox == music.Key)) {
+						AllMusic.Add(new MusicData(music.Value, music.Key, modSource, item.Name));
 					}
 					else {
-						tMusicPlayer.instance.Logger.Info($"Prevented a vanilla overwrite for {name} [#{itemID}]. Selection may play undesired music.");
+						tMusicPlayer.instance.Logger.Info($"Prevented a vanilla overwrite for {name} [#{music.Key}]. Selection may play undesired music.");
 					}
 				}
 			}
@@ -68,41 +173,23 @@ namespace tMusicPlayer
 				tMusicPlayer.instance.Logger.Error($"itemToMusicReference failed and has a null value. Modded music will not be added to the music player.");
 			}
 
-			// Setup UI's item slot count.
-			if (!Main.dedServ) {
-				MusicPlayerUI UI = Instance.MusicUI;
+			MusicUI.SelectionSlots = new MusicBoxSlot[AllMusic.Count];
+			MusicUI.SortedMusicData = new List<MusicData>(AllMusic);
+			MusicUI.OrganizeSelection(initializing: true, SortBy.ID, ProgressBy.None, filterMod: "");
+			MusicUI.DisplayBox = MusicUI.SortedMusicData[0]; // defaults to first music box
 
-				UI.canPlay = new bool[tMusicPlayer.AllMusic.Count];
-
-				if (UI.sortType == SortBy.ID) {
-					tMusicPlayer.AllMusic = tMusicPlayer.AllMusic.OrderBy(x => x.music).ToList();
-				}
-				if (UI.sortType == SortBy.Name) {
-					tMusicPlayer.AllMusic = tMusicPlayer.AllMusic.OrderBy(x => x.name).ToList();
-				}
-				
-				UI.SelectionSlots = new MusicBoxSlot[tMusicPlayer.AllMusic.Count];
-				UI.musicData = new List<MusicData>(tMusicPlayer.AllMusic);
-				UI.OrganizeSelection(SortBy.ID, ProgressBy.None, "", true);
-
-				// Setup the mod list for the Mod Filter
-				// Must occur after all other modded music is established
-				UI.ModList = new List<string>();
-				foreach (MusicData box in UI.musicData) {
-					if (!UI.ModList.Contains(box.mod)) {
-						UI.ModList.Add(box.mod);
-					}
-				}
-				UI.ModList.Sort();
-				UI.ModList.Remove("Terraria");
-				UI.ModList.Remove("Terraria Otherworld");
-				UI.ModList.Insert(0, "Terraria"); // Put Terraria infront of all mods
-				UI.ModList.Insert(1, "Terraria Otherworld"); // Put Terraria Otherworld immediately after
+			// Setup the mod list for the Mod Filter
+			// Must occur after all other modded music is established
+			RegisteredMusic = new Dictionary<string, List<int>> {
+				{ "Terraria", new List<int>() },
+				{ "Terraria Otherworld", new List<int>() }
+			};
+			foreach (MusicData box in MusicUI.SortedMusicData) {
+				RegisteredMusic.TryAdd(box.Mod, new List<int>());
+				RegisteredMusic[box.Mod].Add(box.MusicID);
 			}
-		}
 
-		public override void UpdateUI(GameTime gameTime) {
-			MP_UserInterface?.Update(gameTime);
+			MusicUI.ModList = RegisteredMusic.Keys.ToList();
 		}
 
 		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
@@ -112,23 +199,21 @@ namespace tMusicPlayer
 				layers.Insert(index, new LegacyGameInterfaceLayer(
 					"tMusicPlayer: Music Player",
 					delegate {
-						if (MusicUI.mpToggleVisibility) {
+						if (MusicUI.MusicPlayerVisible)
 							MP_UserInterface.Draw(Main.spriteBatch, new GameTime());
-						}
+
 						return true;
 					},
 					InterfaceScaleType.UI)
 				);
 
-				layers.Insert(++index, new LegacyGameInterfaceLayer("BossChecklist: Custom UI Hover Text",
+				layers.Insert(++index, new LegacyGameInterfaceLayer(
+					"tMusicPlayer: Custom UI Hover Text",
 					delegate {
-						// Detect if the hover text is a single localization key and draw the hover text accordingly
-						if (UIHoverText != "") {
-							string text = UIHoverText.StartsWith("$Mods.") ? Language.GetTextValue(UIHoverText.Substring(1)) : UIHoverText;
-							DrawTooltipBG(Main.spriteBatch, text, UIHoverTextColor);
-						}
-						// Reset text and color back to default state
-						UIHoverText = "";
+						if (!string.IsNullOrEmpty(UIHoverText))
+							DrawTooltipBackground();
+
+						UIHoverText = ""; // Reset text and color back to default state
 						UIHoverTextColor = Main.MouseTextColorReal;
 						return true;
 					},
@@ -137,23 +222,56 @@ namespace tMusicPlayer
 			}
 		}
 
-		private void DrawTooltipBG(SpriteBatch sb, string text, Color textColor = default) {
+		public static string RemoveChatTags(string text) {
+			string editedName = "";
+
+			for (int c = 0; c < text.Length; c++) {
+				// Add each character one by one to find chattags in order
+				// Chat tags cannot be contained inside other chat tags so no need to worry about overlap
+				editedName += text[c];
+				if (editedName.Contains("[i:") && editedName.EndsWith("]")) {
+					// Update return name if a complete item chat tag is found
+					editedName = editedName.Substring(0, editedName.IndexOf("[i:"));
+					continue;
+				}
+				if (editedName.Contains("[i/") && editedName.EndsWith("]")) {
+					// Update return name if a complete item chat tag is found
+					editedName = editedName.Substring(0, editedName.IndexOf("[i/"));
+					continue;
+				}
+				if (editedName.Contains("[c/") && editedName.Contains(':') && editedName.EndsWith("]")) {
+					// Color chat tags are edited differently as we want to keep the text that's nested inside them
+					string part1 = editedName.Substring(0, editedName.IndexOf("[c/"));
+					string part2 = editedName.Substring(editedName.IndexOf(":") + 1);
+					part2 = part2.Substring(0, part2.Length - 1);
+					editedName = part1 + part2;
+					continue;
+				}
+			}
+			return editedName;
+		}
+
+		/// <summary>
+		/// <para>Draws backgrounds for texts similar to the ones used for item tooltips.</para>
+		/// <para>ModifyInterfaceLayers will use this method when hovering over an element that changes the <see cref="UIHoverText"/></para>
+		/// </summary>
+		private void DrawTooltipBackground() {
+			string text = Language.GetTextValue(UIHoverText);
 			if (text == "")
 				return;
 
 			int padd = 20;
-			Vector2 stringVec = FontAssets.MouseText.Value.MeasureString(text);
+			Vector2 stringVec = FontAssets.MouseText.Value.MeasureString(RemoveChatTags(text));
 			Rectangle bgPos = new Rectangle(Main.mouseX + 20, Main.mouseY + 20, (int)stringVec.X + padd, (int)stringVec.Y + padd - 5);
 			bgPos.X = Utils.Clamp(bgPos.X, 0, Main.screenWidth - bgPos.Width);
 			bgPos.Y = Utils.Clamp(bgPos.Y, 0, Main.screenHeight - bgPos.Height);
 
 			Vector2 textPos = new Vector2(bgPos.X + padd / 2, bgPos.Y + padd / 2);
-			if (textColor == default) {
-				textColor = Main.MouseTextColorReal;
-			}
+			if (UIHoverTextColor == null)
+				UIHoverTextColor = Main.MouseTextColorReal; // if no value for color, default to the text color real
 
-			Utils.DrawInvBG(sb, bgPos, new Color(23, 25, 81, 255) * 0.925f);
-			Utils.DrawBorderString(sb, text, textPos, textColor);
+			Utils.DrawInvBG(Main.spriteBatch, bgPos, new Color(23, 25, 81, 255) * 0.925f);
+			Utils.DrawBorderString(Main.spriteBatch, text, textPos, UIHoverTextColor.Value);
 		}
 	}
 }

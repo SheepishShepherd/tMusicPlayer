a5ff832c1a8f82486f24b208515f05b1572eb73e
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2024-07-24 20:58:10 -0400

    using refs cleanup

--- MusicUISystem.cs
+++ MusicUISystem.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.GameContent;
 using Terraria.ID;
